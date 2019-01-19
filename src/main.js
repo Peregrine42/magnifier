ac150e1beef3870a6b137246a51bdd1148199c38
Duncan Fermor <duncan_fermor@fastmail.com>
2019-01-19 23:17:43 +0000

    Stuff and things.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,25 @@ import * as d3 from "d3"
 import range from "./utils/range"
 
 class Point {
-    constructor(x, y) {
+    constructor(x, y, xRange, yRange) {
         this.x = x
         this.y = y
-        this.radius = 10
+        this.radius = 0.1
+
+        this.xRange = xRange
+        this.yRange = yRange
+    }
+
+    x() {
+        return this.xRange(this.x)
+    }
+
+    y() {
+        return this.yRange(this.y)
+    }
+
+    radius() {
+        return this.xRange(this.radius)
     }
 }
 
@@ -13,17 +28,15 @@ class Field {
     constructor(width, height, points) {
         this.width = width
         this.height = height
-        this.points = points.map((p) => {
-            return new Point(p.x * width, p.y * height)
-        })
+        this.points = points
     }
 }
 
 
-function makePoints(max) {
+function makeCoords(max) {
     let result = []
-    for (let i=0; i<max; i++) {
-        result.push(new Point(Math.random(), Math.random()))
+    for (let i = 0; i < max; i++) {
+        result.push([Math.random(), Math.random()])
     }
     return result
 }
@@ -32,11 +45,9 @@ export default function main() {
     console.log("Hello, world!")
 
     const data = [
-        new Field(460, 280, makePoints(3)),
-        new Field(360, 180, makePoints(4))
+        new Field(460, 280, makeCoords(3)),
+        new Field(360, 180, makeCoords(4))
     ]
-
-
 
     d3
         .select(".container")
@@ -49,18 +60,52 @@ export default function main() {
         .attr("height", (field) => {
             return field.height
         })
+        .append("rect")
+        .attr("width", (field) => {
+            return field.width
+        })
+        .attr("height", (field) => {
+            return field.height
+        })
+
+    let scope = d3
+        .select(".container")
+        .selectAll("svg")
+
+    let ranges = scope.nodes()
+        .map((node) => {
+            return node.getBoundingClientRect()
+        })
+        .map((node) => {
+            let xScale = d3.scaleLinear()
+                .domain([0, 1])
+                .range([0, node.width])
+
+            let yScale = d3.scaleLinear()
+                .domain([0, 1])
+                .range([0, node.height])
+            return { x: xScale, y: yScale }
+        })
+
+    scope
         .selectAll("circle")
-        .data((field) => {
-            return field.points
+        .data((field, i) => {
+            const xRange = ranges[i].x
+            const yRange = ranges[i].y
+            let points = field.points.map((coord) => {
+                return { radius: 0.03, x: coord[0], y: coord[1], xRange: xRange, yRange: yRange }
+            })
+            console.log(points)
+            return points
         })
         .enter().append("circle")
         .attr("r", (p) => {
-            return p.radius
+            return p.xRange(p.radius)
         })
         .attr("cx", (p) => {
-            return p.x
+            return p.xRange(p.x)
         })
         .attr("cy", (p) => {
-            return p.y
+            return p.yRange(p.y)
         })
 }
