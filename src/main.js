ea64eb156ab11d404ed5bb834a04a0ff816aece9
Duncan Fermor <duncan_fermor@fastmail.com>
2019-01-19 16:39:10 +0000

    Add d3.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 import eachElement from "./utils/eachElement"
-import { addCanvas, drawBackground } from "./canvas"
-import { drawSprite } from "./sprite"
+import * as d3 from "d3"
 
 export default function main() {
     console.log("Hello, world!")
 
     for (var element of eachElement(".container")) {
-        console.log(element)
-        let canvas = addCanvas(element)
-
-        let ctx = canvas.getContext("2d")
-        drawBackground(ctx)
-        let sprite = drawSprite(ctx, 0.5*canvas.width, 0.5*canvas.height)
     }
 }
